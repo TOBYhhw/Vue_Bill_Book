34bea462b46a4980b5a953e04177a40ad28d6f1a
TOBYhhw <1505994122@qq.com>
2022-06-26 19:57:48 +0800

    修改了一个bug

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -197,6 +197,8 @@ export default {
       //对弹出层中的表单赋值
       console.log(index, row);
       this.form1.Id = row.Id;
+      this.form1.name = row.name;
+      this.form1.money = row.money;
     },
     handleSet1() {
       axios
